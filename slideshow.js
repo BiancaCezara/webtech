3e2d5c835a9536e975a1b9ad1b6456ad961fc601
BiancaCezara <98516537+BiancaCezara@users.noreply.github.com>
2022-04-20 20:47:14 +0300

    Modified js scripts

--- slideshow.js
+++ slideshow.js
@@ -1,42 +1,76 @@
-window.onload = function () {
+
 
     const img1 = "img1";
     const img2 = "img2";
     const img3 = "img3";
-
-
     let images = new Array(img1, img2, img3);
-
     let count = 0;
     let previousCount = images.length - 1;
 
-    const rightArrow = document.getElementById("right-arrow");
-    if (rightArrow) {
-        rightArrow.addEventListener("click", function () {
-            count += 1;
-            previousCount = count - 1;
 
+    function slideOnClick(elementID) {
+
+
+        if (elementID == "right-arrow") {
+            count += 1;
             if (count > 2) {
                 count = 0;
                 previousCount = 2
+            } else {
+                previousCount = count - 1;
             }
-            document.getElementById(images[count]).style.display = "block";
-            document.getElementById(images[previousCount]).style.display = "none";
-        });
-    }
-
-    const leftArrow = document.getElementById("left-arrow");
-
-    if (leftArrow) {
-        leftArrow.addEventListener("click", function () {
-            count -= 1;
-            previousCount = count + 1;
+        }
+        else (elementID == "left-arrow")
+        {   count -= 1;
+            console.log(count);
             if (count < 0) {
                 count = 2;
                 previousCount = 0;
+            } else {
+                previousCount = count + 1;
             }
-            document.getElementById(images[count]).style.display = "block";
-            document.getElementById(images[previousCount]).style.display = "none";
-        });
+        }
+        console.log(count);
+        document.getElementById(images[count]).style.display = "block";
+        document.getElementById(images[previousCount]).style.display = "none";
     }
-}
+
+
+
+
+
+
+
+// const rightArrow = document.getElementById("right-arrow");
+// if (rightArrow) {
+//     rightArrow.addEventListener("click", function () {
+//         count += 1;
+//         previousCount = count - 1;
+//
+//         if (count > 2) {
+//             count = 0;
+//             previousCount = 2
+//         }
+//         document.getElementById(images[count]).style.display = "block";
+//         document.getElementById(images[previousCount]).style.display = "none";
+//     });
+// }
+
+// const leftArrow = document.getElementById("left-arrow");
+
+//     if (leftArrow) {
+//         leftArrow.addEventListener("click", function () {
+//             count -= 1;
+//             previousCount = count + 1;
+//             if (count < 0) {
+//                 count = 2;
+//                 previousCount = 0;
+//             }
+//             document.getElementById(images[count]).style.display = "block";
+//             document.getElementById(images[previousCount]).style.display = "none";
+//         });
+//     }
+// }
+
+// rightArrow.addEventListener("click", function () {
+
